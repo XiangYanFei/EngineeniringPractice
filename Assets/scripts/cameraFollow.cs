04ae5a300bd65f2950165d5e3f9beb017464ae6c
XiangYanFei <470689887@qq.com>
2017-06-23 11:05:59 +0800

    完成摄像机跟随

---
+++ Assets/scripts/cameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+	Vector3 Dir;//摄像机要跟随物体的距离
+	public GameObject Player;//要跟随的物体
+	// Use this for initialization
+	void Start () {
+		Dir.x = Player.transform.position.x - transform.position.x;
+		Dir.y = Player.transform.position.y - transform.position.y-6;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Player.transform.position - Dir;
+	}
+}
