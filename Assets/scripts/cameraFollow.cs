23756bb4426450e1ce5f88342ccfee755f6a50df
XiangYanFei <470689887@qq.com>
2017-06-25 11:45:34 +0800

    设置平板边缘摩擦，调整摄像机范围，未解决窗口显示问题

--- Assets/scripts/cameraFollow.cs
+++ Assets/scripts/cameraFollow.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cameraFollow : MonoBehaviour {
-
+	public int n=3;
 	Vector3 Dir;//摄像机要跟随物体的距离
 	public GameObject Player;//要跟随的物体
 	// Use this for initialization
 	void Start () {
 		Dir.x = Player.transform.position.x - transform.position.x;
-		Dir.y = Player.transform.position.y - transform.position.y-6;
+		Dir.y = Player.transform.position.y - transform.position.y-n;
 	}
 
 	// Update is called once per frame
